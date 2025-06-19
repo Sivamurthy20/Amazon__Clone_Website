17d3d9c2f1b8c908144d912516dab928bcda119b
Sivamurthy20 <sivamurthy222@gmail.com>
2025-06-19 15:49:42 +0000

    setup github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/',
+  base:'/amazon_clone_website',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
