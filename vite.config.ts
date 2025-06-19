ffddd7702483ab7e3c09719bae8b50f653b80893
Sivamurthy20 <sivamurthy222@gmail.com>
2025-06-19 16:51:07 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Amazon_Clone_website/',
+  base:'/Amazon__Clone_Website/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
